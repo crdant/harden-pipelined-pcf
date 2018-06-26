667e372cca1747da0f79f566954fb14f32ee3de3
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-26 18:29:59 -0400

    Adjust access to the opsman IP

--- tasks/preserve-opsman/aws/terraform/opsman.tf
+++ tasks/preserve-opsman/aws/terraform/opsman.tf
@@ -1,11 +1,10 @@
-datasource "aws_instance" "opsman_az1" {
+data "aws_instance" "opsman_az1" {
   filter {
     name   = "tag:Name"
     values = [ "${var.prefix}-OpsMan az1" ]
   }
 }
 
-resource "aws_eip" "opsman" {
-  instance = "${aws_instance.opsman_az1.id}"
-  vpc      = true
+data "aws_eip" "opsman" {
+  public_ip = "${aws_instance.opsman_az1.public_ip}"
 }
