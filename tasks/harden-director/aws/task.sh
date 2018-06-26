3898103013eada8986ed162f5939c87f4e5ea48f
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-26 11:01:31 -0400

    Added director patching from @tariqsiddiqi

---
+++ tasks/harden-director/aws/task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+read -r -d '' iaas_configuration <<EOF
+{
+  "encrypted": true
+}
+EOF
+
+
+om-linux \
+  --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} \
+  --skip-ssl-validation \
+  --username "$OPSMAN_USER" \
+  --password "$OPSMAN_PASSWORD" \
+  configure-director \
+  --iaas-configuration "$iaas_configuration" \
