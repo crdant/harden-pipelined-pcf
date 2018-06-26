51817146da5ea5a2f6b60fce969dd11e111c8005
Chuck D'Antonio <c_dantonio@acm.org>
2018-06-26 11:54:20 -0400

    Polish

--- tasks/harden-director/aws/task.sh
+++ tasks/harden-director/aws/task.sh
@@ -2,17 +2,21 @@
 
 set -eu
 
-read -r -d '' iaas_configuration <<EOF
-{
-  "encrypted": true
+update_director_config () {
+  read -r -d '' iaas_configuration <<EOF
+  {
+    "encrypted": true
+  }
+  EOF
+
+
+  om-linux \
+    --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} \
+    --skip-ssl-validation \
+    --username "$OPSMAN_USER" \
+    --password "$OPSMAN_PASSWORD" \
+    configure-director \
+    --iaas-configuration "$iaas_configuration" \
 }
-EOF
 
-
-om-linux \
-  --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} \
-  --skip-ssl-validation \
-  --username "$OPSMAN_USER" \
-  --password "$OPSMAN_PASSWORD" \
-  configure-director \
-  --iaas-configuration "$iaas_configuration" \
+update_director_config
