fa6db4c6f822bc6d3d3c63dfbde719ea8044c0d8
Chuck D'Antonio <c_dantonio@acm.org>
2018-07-06 09:17:06 -0400

    Prepare for truly standalone pipeline

---
+++ tasks/harden-infrastructure/aws/terraform/bosh_sg_attachment.tf
@@ -0,0 +1,14 @@
+data "aws_instances" "bosh" {
+  instance_tags {
+    deployment = "p-bosh"
+    instance_group = "bosh"
+    job = "bosh"
+  }
+
+  instance_state_names = [ "running", "stopped" ]
+}
+
+resource "aws_network_interface_sg_attachment" "bosh_sg_attachment" {
+  security_group_id    = "${aws_security_group.directorSG.id}"
+  network_interface_id = "${aws_instance.bosh.*.primary_network_interface_id}"
+}
