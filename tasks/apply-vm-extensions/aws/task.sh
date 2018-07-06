4ce4e9da13f8f3000a2a4a4e24e7d9954f49f1f5
Chuck D'Antonio <c_dantonio@acm.org>
2018-07-06 16:06:11 -0400

    Tested implementation of VM extensions for CAPI

--- tasks/apply-vm-extensions/aws/task.sh
+++ tasks/apply-vm-extensions/aws/task.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eu
 
@@ -6,13 +6,13 @@ create_extension() {
   local extension_name=${1}
   local extension_properties=${2}
 
-  om \
-    --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-    --username "$OPS_MGR_USR" \
-    --password "$OPS_MGR_PWD" \
+  om-linux \
+    --target "$opsman_target" \
+    --username "$opsman_user" \
+    --password "$opsman_password" \
     --skip-ssl-validation \
     create-vm-extension \
-    --name "${extension_name}"
+    --name "${extension_name}" \
     --cloud-properties "${extension_properties}"
 }
 
@@ -22,10 +22,10 @@ add_extension() {
   local extension_name="${3}"
 
   product_guid=$(
-    om \
-      --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-      --username "$OPS_MGR_USR" \
-      --password "$OPS_MGR_PWD" \
+    om-linux \
+      --target "$opsman_target" \
+      --username "$opsman_user" \
+      --password "$opsman_password" \
       --skip-ssl-validation \
       curl \
       --path /api/v0/staged/products \
@@ -34,10 +34,10 @@ add_extension() {
   )
 
   job_guid=$(
-    om \
-      --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-      --username "$OPS_MGR_USR" \
-      --password "$OPS_MGR_PWD" \
+    om-linux \
+      --target "$opsman_target" \
+      --username "$opsman_user" \
+      --password "$opsman_password" \
       --skip-ssl-validation \
       curl \
       --path /api/v0/staged/products/${product_guid}/jobs \
@@ -46,10 +46,10 @@ add_extension() {
   )
 
   resources=$(
-    om \
-      --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-      --username "$OPS_MGR_USR" \
-      --password "$OPS_MGR_PWD" \
+    om-linux \
+      --target "$opsman_target" \
+      --username "$opsman_user" \
+      --password "$opsman_password" \
       --skip-ssl-validation \
       curl \
       --path /api/v0/staged/products/${product_guid}/jobs/${job_guid}/resource_config \
@@ -57,21 +57,23 @@ add_extension() {
       jq ".additional_vm_extensions = [ \"${extension_name}\" ]"
   )
 
-  om \
-    --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-    --username "$OPS_MGR_USR" \
-    --password "$OPS_MGR_PWD" \
+  echo "Creating extentions on ${product} (guid: ${product_guid}), jobs ${job} (guid: ${job_guid})..."
+  om-linux \
+    --target "$opsman_target" \
+    --username "$opsman_user" \
+    --password "$opsman_password" \
     --skip-ssl-validation \
     curl \
-    --path /api/v0/staged/products/${product_guid}/jobs/${job_guid}/resource_config
+    --path /api/v0/staged/products/${product_guid}/jobs/${job_guid}/resource_config \
     --request PUT \
-    --data "${resources}"
+    --data "${resources}" \
+    --silent
 
 }
 
-get_security_group_id() {
+get_terraform_output() {
   output_name=${1}
-  terraform output "${output_name}"
+  terraform output --state terraform-state/terraform.tfstate "${output_name}"
 }
 
 apply_security_group() {
@@ -81,28 +83,23 @@ apply_security_group() {
 
   vm_extension=$(
     jq -n \
-      --arg product ${product}
-      --arg job ${job}
-      --arg security_group_id ${security_group_id}
+      --arg name "${product}_${job}_sg" \
+      --arg security_group_id ${security_group_id} \
       '{
-        "name": "$product_$job_sg",
+        "name": $name,
         "cloud_properties": {
           "security_groups": [
-            "$security_group_id"
+            $security_group_id
           ]
         }
       }'
   )
-  create_extension "${product}_${job}_sg" "${bosh_extension}"
+  create_extension "${product}_${job}_sg" "${vm_extension}"
   add_extension "${product}" "${job}" "${product}_${job}_sg"
-
 }
 
 main () {
-  bosh_security_group="$(get_security_group_id "director_security_group")"
-  apply_security_group "p-bosh" "director" "${bosh_security_group}"
-
-  capi_security_group="$(get_security_group_id "cloud_controller_security_group")"
+  capi_security_group="$(get_terraform_output "cloud_controller_security_group_id")"
   apply_security_group "cf" "cloud_controller" "${capi_security_group}"
 }
 
