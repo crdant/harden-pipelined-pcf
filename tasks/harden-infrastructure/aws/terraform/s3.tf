aad1ea4b4dfe2b164143d412e49c6fac1351eb51
Chuck D'Antonio <c_dantonio@acm.org>
2018-07-06 10:25:36 -0400

    Gather data about existing buckets

--- tasks/harden-infrastructure/aws/terraform/s3.tf
+++ tasks/harden-infrastructure/aws/terraform/s3.tf
@@ -7,102 +7,22 @@ resource "aws_kms_key" "blobstore_encryption" {
   }
 }
 
-resource "aws_s3_bucket" "bosh" {
-    bucket = "${var.prefix}-bosh"
-    acl = "private"
-    force_destroy= true
-
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = "${aws_kms_key.blobstore_encryption.arn}"
-          sse_algorithm     = "aws:kms"
-        }
-      }
-    }
-
-    tags {
-        Name = "${var.prefix}-bosh"
-        Environment = "${var.prefix}"
-    }
+data "aws_s3_bucket" "bosh" {
+  bucket = "${var.prefix}-bosh"
 }
 
-resource "aws_s3_bucket" "buildpacks" {
-    bucket = "${var.prefix}-buildpacks"
-    acl = "private"
-    force_destroy= true
-
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = "${aws_kms_key.blobstore_encryption.arn}"
-          sse_algorithm     = "aws:kms"
-        }
-      }
-    }
-
-    tags {
-        Name = "${var.prefix}-buildpacks"
-        Environment = "${var.prefix}"
-    }
+data "aws_s3_bucket" "buildpacks" {
+  bucket = "${var.prefix}-buildpacks"
 }
 
-resource "aws_s3_bucket" "droplets" {
-    bucket = "${var.prefix}-droplets"
-    acl = "private"
-    force_destroy= true
-
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = "${aws_kms_key.blobstore_encryption.arn}"
-          sse_algorithm     = "aws:kms"
-        }
-      }
-    }
-
-    tags {
-        Name = "${var.prefix}-droplets"
-        Environment = "${var.prefix}"
-    }
+data "aws_s3_bucket" "droplets" {
+  bucket = "${var.prefix}-droplets"
 }
 
-resource "aws_s3_bucket" "packages" {
-    bucket = "${var.prefix}-packages"
-    acl = "private"
-    force_destroy= true
-
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = "${aws_kms_key.blobstore_encryption.arn}"
-          sse_algorithm     = "aws:kms"
-        }
-      }
-    }
-
-    tags {
-        Name = "${var.prefix}-packages"
-        Environment = "${var.prefix}"
-    }
+data "aws_s3_bucket" "packages" {
+  bucket = "${var.prefix}-packages"
 }
 
-resource "aws_s3_bucket" "resources" {
-    bucket = "${var.prefix}-resources"
-    acl = "private"
-    force_destroy= true
-
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = "${aws_kms_key.blobstore_encryption.arn}"
-          sse_algorithm     = "aws:kms"
-        }
-      }
-    }
-    
-    tags {
-        Name = "${var.prefix}-resources"
-        Environment = "${var.prefix}"
-    }
+data "aws_s3_bucket" "resources" {
+  bucket = "${var.prefix}-resources"
 }
