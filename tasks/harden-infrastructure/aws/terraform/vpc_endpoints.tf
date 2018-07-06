4c7785995019c0835c03cc253d0a5349f93cf024
Chuck D'Antonio <c_dantonio@acm.org>
2018-07-06 10:33:17 -0400

    Refresh for current naming

--- tasks/harden-infrastructure/aws/terraform/vpc_endpoints.tf
+++ tasks/harden-infrastructure/aws/terraform/vpc_endpoints.tf
@@ -1,28 +1,28 @@
 resource "aws_vpc_endpoint" "ec2" {
-  vpc_id            = "${aws_vpc.PcfVpc.id}"
+  vpc_id            = "${data.aws_vpc.pcf_vpc.id}"
   service_name      = "com.amazonaws.${var.aws_region}.ec2"
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [
-    "${aws_security_group.directorSG.id}"
+    "${data.aws_security_group.opsman_security_group.id}"
   ]
 
   subnet_ids = [
-    "${aws_subnet.PcfVpcInfraSubnet_az1.id}"
+    "${aws_subnet.pcf_infra_az1.id}"
   ]
 
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id             = "${aws_vpc.PcfVpc.id}"
+  vpc_id             = "${data.aws_vpc.pcf_vpc.id}"
   service_name       = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type  = "Gateway"
-  
+
   route_table_ids    = [
-      "${aws_route_table.PublicSubnetRouteTable.id}",
-      "${aws_route_table.PrivateSubnetRouteTable_az1.id}",
-      "${aws_route_table.SubnetRouteTable_az2.id}",
-      "${aws_route_table.SubnetRouteTable_az3.id}"
-    ]
+    "${data.aws_route_table.pcf_public.id}",
+    "${data.aws_route_table.pcf_private_az1.id}",
+    "${data.aws_route_table.pcf_private_az2.id}",
+    "${data.aws_route_table.pcf_private_az3.id}"
+  ]
 }
